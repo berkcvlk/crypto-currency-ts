bda22de925fe21c8c8d81360a331ea781588ec28
Berk Civelek <berkcvlk@gmail.com>
2021-06-11 22:53:04 +0300

    Api imports changed

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,7 @@
 import axios from "axios";
 
+import * as cons from "../constants";
 import { urlBuilder } from "../utils";
-import {
-  BASE_URL,
-  MARKET_CAP_ORDER,
-  COIN_PER_PAGE,
-  PAGE,
-  CURRENCY,
-  SPARKLINE,
-} from "../constants";
 
 export const getCoins = async () => {
   // getCoinsUrlByMarketCap returns related url for
@@ -16,15 +9,15 @@ export const getCoins = async () => {
   // "constants" file.
   // This can be changed!
   const params = {
-    vs_currency: CURRENCY,
-    order: MARKET_CAP_ORDER,
-    per_page: COIN_PER_PAGE,
-    page: PAGE,
-    sparkline: SPARKLINE,
+    vs_currency: cons.CURRENCY,
+    order: cons.MARKET_CAP_ORDER,
+    per_page: cons.COIN_PER_PAGE,
+    page: cons.PAGE,
+    sparkline: cons.SPARKLINE,
   };
 
   const response = await axios.get(
-    urlBuilder(BASE_URL, "/coins/markets", params)
+    urlBuilder(cons.BASE_URL, "/coins/markets", params)
   );
   const data = await response.data;
 
