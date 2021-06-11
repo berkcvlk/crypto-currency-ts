ab8a85b085e66dd9bfa32bef21e52a9281f080e3
Berk Civelek <berkcvlk@gmail.com>
2021-06-11 22:38:40 +0300

    Supported currencies added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,17 @@ export const COIN_PER_PAGE = 21;
 export const PAGE = 1;
 export const CURRENCY = "usd";
 export const MARKET_CAP_ORDER = "market_cap_desc";
+
+// If this is true, api will return last 7 days
+// data for graphic
 export const SPARKLINE = false;
+
+// This list can be get from
+// ...api/v3/simple/supported_vs_currencies
+// But, for now, just support these.
+export enum currencies {
+  "usd" = "$",
+  "eur" = "€",
+  "gbp" = "£",
+  "try" = "₺",
+}
