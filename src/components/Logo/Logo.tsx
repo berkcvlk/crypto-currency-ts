7e3bd1442aad4494019de73d05a44117677b38fc
Berk Civelek <berkcvlk@gmail.com>
2021-06-11 21:39:17 +0300

    Logo created

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import { Div, Image, Text } from "./styles";
+
+const Logo = () => {
+  return (
+    <Div>
+      <Image src="/assets/images/logo.png" />
+      <Text>Chain</Text>
+    </Div>
+  );
+};
+
+export default Logo;
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 290px;
+  height: 290px;
+
+  grid-column: 1 / 3;
+  grid-row: 1 / 3;
+
+  background-color: var(--faded-color);
+  border-radius: 25px;
+`;
+
+export const Image = styled.img`
+  width: 90px;
+  height: 90px;
+
+  margin-bottom: 12px;
+`;
+
+export const Text = styled.span`
+  font-size: 32px;
+  font-weight: 800;
+
+  color: var(--primary-color);
+
+  letter-spacing: 5px;
+  text-transform: uppercase;
+`;
