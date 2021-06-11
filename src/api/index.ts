cb7846f7e7548ba14fe4f06843318373595f6e79
Berk Civelek <berkcvlk@gmail.com>
2021-06-11 22:54:39 +0300

    Api comments updated

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,10 +4,6 @@ import * as cons from "../constants";
 import { urlBuilder } from "../utils";
 
 export const getCoins = async () => {
-  // getCoinsUrlByMarketCap returns related url for
-  // the coin list according to the constants in the
-  // "constants" file.
-  // This can be changed!
   const params = {
     vs_currency: cons.CURRENCY,
     order: cons.MARKET_CAP_ORDER,
@@ -16,6 +12,9 @@ export const getCoins = async () => {
     sparkline: cons.SPARKLINE,
   };
 
+  // urlBuilder returns related url for
+  // the coin list according to the constants in the
+  // "constants" file. This can be changed!
   const response = await axios.get(
     urlBuilder(cons.BASE_URL, "/coins/markets", params)
   );
