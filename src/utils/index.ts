012953a0f5c294379d480dd848767564f7f97721
Berk Civelek <berkcvlk@gmail.com>
2021-06-11 22:39:18 +0300

    urlBuilder added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,19 @@
-import {
-  BASE_URL,
-  MARKET_CAP_ORDER,
-  COIN_PER_PAGE,
-  PAGE,
-  CURRENCY,
-  SPARKLINE,
-} from "../constants";
+import { CURRENCY, currencies } from "../constants";
 
-export const getCoinsUrlByMarketCap = () => {
-  const PATH = "/coins/markets";
-  return `${
-    BASE_URL + PATH
-  }?vs_currency=${CURRENCY}&order=${MARKET_CAP_ORDER}&per_page=${COIN_PER_PAGE}&page=${PAGE}&sparkline=${SPARKLINE}`;
+export const urlBuilder = (base: string, path: string, params?: {}) => {
+  if (!path.includes("?") && params) {
+    path = path + "?";
+  }
+
+  // Add the query params if there is
+  // params object
+  const query = params
+    ? Object.entries(params).reduce((q, v) => q + `&${v[0] + "=" + v[1]}`, "")
+    : "";
+
+  return base + path + query;
+};
+
+export const currencyPicker = () => {
+  return currencies[CURRENCY];
 };
