d3ab58b1dad9a7b416242d80e41bc9dea3b2dfbd
Berk Civelek <berkcvlk@gmail.com>
2021-06-11 22:39:10 +0300

    Api call refactored according to urlBuilder

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,31 @@
 import axios from "axios";
-import { getCoinsUrlByMarketCap } from "../utils";
+
+import { urlBuilder } from "../utils";
+import {
+  BASE_URL,
+  MARKET_CAP_ORDER,
+  COIN_PER_PAGE,
+  PAGE,
+  CURRENCY,
+  SPARKLINE,
+} from "../constants";
 
 export const getCoins = async () => {
   // getCoinsUrlByMarketCap returns related url for
   // the coin list according to the constants in the
   // "constants" file.
   // This can be changed!
-  const response = await axios.get(getCoinsUrlByMarketCap());
+  const params = {
+    vs_currency: CURRENCY,
+    order: MARKET_CAP_ORDER,
+    per_page: COIN_PER_PAGE,
+    page: PAGE,
+    sparkline: SPARKLINE,
+  };
+
+  const response = await axios.get(
+    urlBuilder(BASE_URL, "/coins/markets", params)
+  );
   const data = response.data;
 
   return data;
