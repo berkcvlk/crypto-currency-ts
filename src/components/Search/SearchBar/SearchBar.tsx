166ab20a8656068bf3e59b80a44f43084b24cd43
Berk Civelek <berkcvlk@gmail.com>
2021-06-16 21:18:42 +0300

    OnFocus added to SearchBar comp

--- src/components/Search/SearchBar/SearchBar.tsx
+++ src/components/Search/SearchBar/SearchBar.tsx
@@ -3,11 +3,12 @@ import { FC } from "react";
 import { Input } from "./styles";
 
 interface Props {
+  onFocus: () => void;
   placeholder: string;
 }
 
-const SearchBar: FC<Props> = ({ placeholder }) => {
-  return <Input placeholder={placeholder}></Input>;
+const SearchBar: FC<Props> = ({ onFocus, placeholder }) => {
+  return <Input onFocus={onFocus} placeholder={placeholder}></Input>;
 };
 
 export default SearchBar;
