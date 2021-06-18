4247419029fb76157501de472f0ecfe9138850b1
Berk Civelek <berkcvlk@gmail.com>
2021-06-18 15:09:09 +0300

    Added state to the SearchBar, handled state func

--- src/components/Search/SearchBar/SearchBar.tsx
+++ src/components/Search/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import React, { FC } from "react";
 
 import { Input } from "./styles";
 
@@ -7,11 +7,30 @@ interface Props {
   // show the search field which contains
   // results that get by searchText
   onShowSearchField: () => void;
+
+  // Object contains search text state and
+  // state set function handler
+  searchStateProps: { state: string; set: (text: string) => void };
   placeholder: string;
 }
 
-const SearchBar: FC<Props> = ({ onShowSearchField, placeholder }) => {
-  return <Input onFocus={onShowSearchField} placeholder={placeholder}></Input>;
+const SearchBar: FC<Props> = ({
+  onShowSearchField,
+  placeholder,
+  searchStateProps,
+}) => {
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    searchStateProps.set(e.target.value);
+  };
+
+  return (
+    <Input
+      value={searchStateProps.state}
+      onFocus={onShowSearchField}
+      onChange={onChangeHandler}
+      placeholder={placeholder}
+    ></Input>
+  );
 };
 
 export default SearchBar;
