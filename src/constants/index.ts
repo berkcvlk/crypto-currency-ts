6a2e5a5c5857c25d94cb562d5c9544b829b25023
Berk Civelek <berkcvlk@gmail.com>
2021-06-18 22:19:15 +0300

    Coin per page changed to 41

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://api.coingecko.com/api/v3";
-export const COIN_PER_PAGE = 21; // Default's 21.
+export const COIN_PER_PAGE = 41;
 export const PAGE = 1;
 export const CURRENCY = "usd";
 export const MARKET_CAP_ORDER = "market_cap_desc";
